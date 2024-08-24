59aeb493329a44e238c5f993ba167f8d65e4d4dc
Henri Galdámez <estuardo.galdamezedumail.com>
2024-08-24 14:55:28 -0600

    Despliegue

---
+++ src/main/java/com/depliege/ProyectoIntegradorDespliege/ProyectoIntegradorDespliegeApplication.java
@@ -0,0 +1,13 @@
+package com.depliege.ProyectoIntegradorDespliege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntegradorDespliegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIntegradorDespliegeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/depliege/ProyectoIntegradorDespliege/ProyectoIntegradorDespliegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depliege.ProyectoIntegradorDespliege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegradorDespliegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
